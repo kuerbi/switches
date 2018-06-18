847f2ff41e02edeb44edde1aa88455432ef709f4
Michael Golenia <michael.golenia2@gmail.com>
2018-06-18 15:28:19 +0200

    Add style for the Header and Title

--- source/header.jsx
+++ source/header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div class={"header"}>
         <h1 class={"title"}>Switches</h1>
       </div>
     )
