1f2221490f418a181080bb9a70ffa041cf42d597
Michael Golenia <michael.golenia2@gmail.com>
2018-06-18 16:06:02 +0200

    Show correct Title depending on game state

--- source/header.jsx
+++ source/header.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
 
-export default class Header extends Component {
+export class Header extends Component {
+  renderTitle() {
+      const { gameState } = this.props;
+
+      if (gameState === "not_running") {
+          return "Switches"
+      } else if(gameState === "running") {
+          return "Tap a switch"
+      }
+  }
+
   render() {
     return (
-      <div class={"header"}>
-        <h1 class={"title"}>Switches</h1>
+      <div className={"header"}>
+        <h1 className={"title"}>{this.renderTitle()}</h1>
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+    return {
+        gameState: state.gameState
+    }
+}
+
+export default connect(mapStateToProps)(Header);
--- test/header_test.jsx
+++ test/header_test.jsx
@@ -2,12 +2,22 @@ import React from "react"
 import { expect } from "chai"
 import { shallow } from "enzyme"
 
-import Header from "header"
+import { Header } from "header"
 
 describe("Header component", () => {
-  it("displays the correct title", () => {
-    const wrapper = shallow(<Header />);
+  context("when game is not running", () => {
+    it("displays the correct title", () => {
+        const wrapper = shallow(<Header gameState={"not_running"}/>);
+    
+        expect(wrapper.find("h1").text()).equal("Switches");
+      });
+  });
 
-    expect(wrapper.find("h1").text()).equal("Switches");
+  context("when game is running", () => {
+    it("displays the correct title", () => {
+        const wrapper = shallow(<Header gameState={"running"}/>);
+    
+        expect(wrapper.find("h1").text()).equal("Tap a switch");
+      });
   });
 });
