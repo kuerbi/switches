775a7f2b19c5504726c2c05633abfec87082e6d6
Michael Golenia <michael.golenia2@gmail.com>
2018-06-19 12:50:31 +0200

    Add Gamefield Component

---
+++ source/gamefield.jsx
@@ -0,0 +1,12 @@
+//@flow
+import React, { Component } from "react"
+
+
+type Props = {};
+export class GameField extends Component<Props> {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
---
+++ test/gamefield_test.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { expect } from "chai"
+import { mount } from "enzyme"
+import { GameField } from "gamefield"
+
+
+describe("Gamefield component", () => {
+  it("shows 25 switches TODO", () => {
+    const wrapper = mount(<GameField />);
+  });
+});
