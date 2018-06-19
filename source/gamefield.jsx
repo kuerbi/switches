457bd26a4dfe58e060eb69903480f957c89c1043
Michael Golenia <michael.golenia2@gmail.com>
2018-06-19 14:07:14 +0200

    Show the pattern on the game field

--- source/gamefield.jsx
+++ source/gamefield.jsx
@@ -2,19 +2,28 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 
-
 type Props = {
   fields: any
 };
 export class GameField extends Component<Props> {
+  onFieldClick(r: number, c: number) {
+    console.log("click on field", r, c);
+  }
+
   render() {
     const { fields } = this.props;
 
     let patterns = [];
 
-    for(let r = 0; r < fields.length; r++) {
-      for(let c = 0; c < fields[0].length; c++) {
-        patterns.push(<div className={"pattern"}></div>);
+    for(let r: number = 0; r < fields.length; r++) {
+      for(let c: number = 0; c < fields[0].length; c++) {
+        patterns.push(
+          <div 
+            key={"pattern_" + r + c} 
+            className={"pattern" + ((fields[r][c] == 1) ? " pattern--on":"")} 
+            onClick={this.onFieldClick.bind(this, r, c)}>
+          </div>
+        );
       }
     }
 
--- test/gamefield_test.jsx
+++ test/gamefield_test.jsx
@@ -5,9 +5,15 @@ import { GameField } from "gamefield"
 import { startingPatterns } from "constants/patterns"
 
 describe("Gamefield component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<GameField fields={startingPatterns[0]} />);
+  })
+
   it("shows 25 switches", () => {
-    const wrapper = mount(<GameField fields={startingPatterns[0]} />);
 
     expect(wrapper.find(".pattern")).to.have.length(25);
   });
+
 });
