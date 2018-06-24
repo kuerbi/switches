7b5f537f2aa9a61e4e216ec0f93b55948e220aec
Michael Golenia <michael.golenia2@gmail.com>
2018-06-24 08:43:31 +0200

    Add prop initialState

--- source/components/root.jsx
+++ source/components/root.jsx
@@ -3,11 +3,18 @@ import * as React from "react"
 import { Provider } from "react-redux"
 import { createStore } from "redux"
 import reducer, { initialState } from "state/reducers"
+import type { State } from "state/reducers"
+
 
 type Props = {
-  children: any
+  children: any,
+  initialState?: State
 }
 export default class Root extends React.Component<Props>{
+  static defaultProps = {
+    initialState
+  }
+
   render() {
     const store = createStore(reducer, initialState);
 
