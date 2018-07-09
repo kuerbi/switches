b161889ecb52bcb305c5bb9f5914757ec5b79c9b
Michael Golenia <michael.golenia2@gmail.com>
2018-07-09 20:37:13 +0200

    Add tests for header
    - click on abort game
    - click on restart game

--- test/header_test.jsx
+++ test/header_test.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { expect } from "chai"
 import { shallow } from "enzyme"
+import sinon from "sinon"
 
 import { Header } from "components/header"
 
@@ -15,19 +16,54 @@ describe("Header component", () => {
   
   context("when game is running", () => {
     let wrapper;
+    let spyAbortGame = sinon.spy()
 
     beforeEach(() => {
-      wrapper = shallow(<Header game={"running"}/>);
+      wrapper = shallow(<Header game={"running"} abortGame={spyAbortGame}/>);
     });
 
     it("displays the correct title", () => {      
       expect(wrapper.find("h1").text()).equal("Tap a switch");
     });
 
-    // TODO:
-    it("shows abort link", () => {
-      // expect(wrapper.find(".button .button--action").text()).equal("Abort");
+    describe("click on abort", () => {
+      beforeEach(() => {
+        wrapper.find(".header__action > .action").simulate("click", {
+          preventDefault: () => {}
+        });
+      })
+
+      it("should call abortGame", () => {
+        expect(spyAbortGame).to.have.property("callCount", 1)
+      })
     });
+
+  });
+
+  context("when game is victory", () => {
+    let wrapper;
+    let spyRestartGame = sinon.spy()
+
+    beforeEach(() => {
+      wrapper = shallow(<Header game={"victory"} restartGame={spyRestartGame}/>);
+    });
+
+    it("displays the correct title", () => {      
+      expect(wrapper.find("h1").text()).equal("Victory");
+    });
+
+    describe("click on restart", () => {
+      beforeEach(() => {
+        wrapper.find(".header__action > .action").simulate("click", {
+          preventDefault: () => {}
+        });
+      })
+
+      it("should call restartGame", () => {
+        expect(spyRestartGame).to.have.property("callCount", 1)
+      })
+    });
+
   });
 
   context("when game is won", () => {
